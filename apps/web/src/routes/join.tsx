d87f1a8bc405a9fab508b9d915e9fbae942fffdb
mameta1212 <mame1212mame@gmail.com>
2025-06-29 15:46:27 +0900

    feat: create.tsxとjoin.tsxにメニューアイコンを追加

--- apps/web/src/routes/join.tsx
+++ apps/web/src/routes/join.tsx
@@ -1,12 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router';
+import ViewListIcon from '@mui/icons-material/ViewList';
 
 const Join: React.FC = () => {
   return (
     <div className='common_background_layout'>
       {/* ヘッダーセクション */}
-      {/* あとでメニューを追加実装する */}
       <header className='common_header_layout'>
+        <button className='common_header_button_menu'>
+          <ViewListIcon />
+        </button>
+
         <h1 className='common_header_text_title'>R-Streamに参加する</h1>
+        {/* 右側のスペーサー（左側のメニューアイコン分のスペースを置き、左右対称にする) */}
+        <div className='common_header_content_spacer'></div>
       </header>
 
       <main className='common_main_layout_center'>
